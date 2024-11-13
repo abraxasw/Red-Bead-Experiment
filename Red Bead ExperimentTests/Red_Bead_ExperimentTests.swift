c33936820a5868b96e45138ae45b0af1d8783e64
Andrew Tam <me@andrewhtam.com>
2024-11-13 22:57:27 +1100

    Initial Commit

---
+++ Red Bead ExperimentTests/Red_Bead_ExperimentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Red_Bead_ExperimentTests.swift
+//  Red Bead ExperimentTests
+//
+//  Created by Andrew Tam on 13/11/2024.
+//
+
+import Testing
+@testable import Red_Bead_Experiment
+
+struct Red_Bead_ExperimentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
